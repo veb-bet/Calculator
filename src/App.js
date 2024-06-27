b47fd4ba1e670b33781331dbf36f845d6e9d0812
veb-bet <73333734+veb-bet@users.noreply.github.com>
2024-06-27 17:03:48 +0000

    Добавлены файлы приложения и запуска

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Calculator from "./Calculator";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Калькулятор</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
